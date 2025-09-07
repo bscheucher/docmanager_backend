d4613739f180c2c41bc5923132ac8710da18dd2e
bscheucher <bernhard.scheucher@gmail.com>
2025-09-07 20:53:32 +0200

    modify BaseEntity and Tag

--- src/main/java/com/app/docmanager/entity/BaseEntity.java
+++ src/main/java/com/app/docmanager/entity/BaseEntity.java
@@ -1,4 +1,30 @@
 package com.app.docmanager.entity;
 
-public class BaseEntity {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
--- src/main/java/com/app/docmanager/entity/Tag.java
+++ src/main/java/com/app/docmanager/entity/Tag.java
@@ -1,4 +1,46 @@
 package com.app.docmanager.entity;
 
-public class Tag {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tags")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true, onlyExplicitlyIncluded = true)
+@ToString(exclude = "documents")
+public class Tag extends BaseEntity {
+
+    @NotBlank(message = "Tag name cannot be blank")
+    @Size(max = 100, message = "Tag name cannot exceed 100 characters")
+    @Column(name = "name", unique = true, nullable = false, length = 100)
+    @EqualsAndHashCode.Include
+    private String name;
+
+    @ManyToMany(mappedBy = "tags", fetch = FetchType.LAZY)
+    @ToString.Exclude
+    private Set<Document> documents = new HashSet<>();
+
+    // Constructor for just name
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    // Utility methods for managing relationships
+    public void addDocument(Document document) {
+        this.documents.add(document);
+        document.getTags().add(this);
+    }
+
+    public void removeDocument(Document document) {
+        this.documents.remove(document);
+        document.getTags().remove(this);
+    }
 }
